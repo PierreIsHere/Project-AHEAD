ffdb842aab1f8c6a27a25e1895a3ecbfaff1844c
antimonyB <anthony.bertnyk@gmail.com>
2019-09-15 02:35:35 -0400

    Fix python to work on windows

--- main.py
+++ main.py
@@ -10,29 +10,29 @@ from selenium.webdriver.common.keys import Keys
 #cont - wether or not the the input is continuous
 #side direction of video scrubbing
 #bar = location of video bar
-def scrub(cont,side,bar):	
-	if(!cont):
-		if(side)
-			bar.send_keys(Keys.ARROW_RIGHT)
-		else:
-			bar.send_keys(Keys.ARROW_LEFT)
-	else:
-		if(side):
-			while(minRoll <=angle <= maxRoll):
-			browser.send_keys(Keys.ARROW_RIGHT)
-		else:
-			while(minRoll <=angle <= maxRoll):
-			browser.send_keys(Keys.ARROW_LEFT) 
+def scrub(cont,side,bar):   
+    if(cont == False):
+        if(side):
+            bar.send_keys(Keys.ARROW_RIGHT)
+        else:
+            bar.send_keys(Keys.ARROW_LEFT)
+    else:
+        if(side):
+            while(minRoll <=angle <= maxRoll):
+                browser.send_keys(Keys.ARROW_RIGHT)
+        else:
+            while(minRoll <=angle <= maxRoll):
+                browser.send_keys(Keys.ARROW_LEFT) 
 
 #buttons can be next,prev,(fullscreen)/minimize,(play)/pause
 #broswer defines the action location
 def buttonClick(button,browser):
-	browser.find_element_by_class_name('ytp-'+button+'-button').click()
+    browser.find_element_by_class_name('ytp-'+button+'-button').click()
          
 #Initialization
-ser = serial.Serial('/dev/ttyACM0', 9600)
+ser = serial.Serial('COM6', 9600)
 
-browser = webdriver.Firefox()
+browser = webdriver.Chrome("chromedriver.exe")
 browser.get('https://www.youtube.com/watch?v=j8umNI8gtlA')
 
 data = []
@@ -48,25 +48,25 @@ while True:
     string = string_n.rstrip()
     print(string)
     #re initializing the array to save on ram only updating when accelerometer recalibrates
-    if(string[0] == "0")
-    	data = []
-    	temp=string.split(",")
-    	data.append([int(temp[0]),int(temp[1]),int(temp[2])])	
-	else:
-		temp=string.split(",")
-    	data.append([int(temp[0]),int(temp[1]),int(temp[2])])
+    if(string[0] == "0"):
+        data = []
+        temp=string.split(",")
+        data.append([int(float(temp[0])),int(float(temp[1])),int(float(temp[2]))])   
+    else:
+        temp=string.split(",")
+        data.append([int(float(temp[0])),int(float(temp[1])),int(float(temp[2]))])
     bar = browser.find_element_by_class_name("ytp-progress-bar ")
-    if(head turn right):
-    	scrub(0,1,bar)
-    elif(head turn left):
-    	scrub(0,0,bar)
-    elif(head look up):
-    	buttonClick("play",browser)
-    elif(swivel right):
-    	buttonClick("next",browser)
-    elif(swivel left):
-    	buttonClick("prev",browser)
-    elif(look down):
-    	buttonClick("fullscreen",browser)
-ser.close()
-browser.close()
+    if(data[0][0]>35):
+        scrub(0,1,bar)
+    elif(data[0][0]>35):
+        scrub(0,0,bar)
+    elif(data[0][0]>35):
+        buttonClick("play",browser)
+    elif(data[0][0]>35):
+        buttonClick("next",browser)
+    elif(data[0][0]>35):
+        buttonClick("prev",browser)
+    elif(data[0][0]>35):
+        buttonClick("fullscreen",browser)
+#ser.close()
+#browser.close()
