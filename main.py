1c9e5b0e5eb849e81512c4818aabde825ae135e2
antimonyB <anthony.bertnyk@gmail.com>
2019-09-15 06:53:00 -0400

    Improved gesture recognition

--- main.py
+++ main.py
@@ -42,6 +42,7 @@ data = []
 
 #Main Loop
 #Yaw,Pitch,Roll
+currentGest=0;
 while True:
     b = ser.readline()
     string_n = b.decode()
@@ -52,21 +53,38 @@ while True:
         data = []
         temp=string.split(",")
         data.append([int(float(temp[0])),int(float(temp[1])),int(float(temp[2]))])   
-    else:
-        temp=string.split(",")
-        data.append([int(float(temp[0])),int(float(temp[1])),int(float(temp[2]))])
     bar = browser.find_element_by_class_name("ytp-progress-bar ")
-    if(data[0][2]>15):
-        scrub(0,1,bar)
-    elif(data[0][2]<-15):
-        scrub(0,0,bar)
-    elif(data[0][1]>15):
-        buttonClick("play",browser)
+    
+    if(currentGest != 0 and -10<float(data[0][1])<10 and -10<float(data[0][2])<10):
+        print("Centered")
+        if(currentGest==3): #up
+            print("Play/Pause")
+            buttonClick("play",browser)
+        elif(currentGest==4): #down
+            print("FULLSCREEN")
+            buttonClick("fullscreen",browser)
+        elif(currentGest==5): #roll left
+            scrub(0,0,bar)
+            print("Skip forward")
+        elif(currentGest==6): #roll right
+            scrub(0,1,bar)
+            print("Skip backward")
+        
+        currentGest=0
+    
+    if(data[0][2]>15): #roll right
+        currentGest=6
+    elif(data[0][2]<-15): #roll left
+        currentGest=5
+    elif(data[0][1]>15): #up
+        currentGest=3
     #elif(data[0][1]<-15):
     #    buttonClick("next",browser)
     #elif(data[0][0]>35):
     #    buttonClick("prev",browser)
-    elif(data[0][1]<-15):
-        buttonClick("fullscreen",browser)
+    elif(data[0][1]<-15): #down
+        
+        currentGest=4
+        
 #ser.close()
 #browser.close()
